872e2bef364c3b049c81c45c452dc70decc5bb49
Nicolás Ruiz <nruizbl@unal.edu.co>
2026-09-19 07:31:35 -0500

    first clases

--- PRE_02_mapreduce/tests/test_src.py
+++ PRE_02_mapreduce/tests/test_src.py
@@ -9,9 +9,10 @@ OUTPUT_FOLDER = "PRE_02_mapreduce/temp/output"
 
 def test_01():
 
+    n = 1000
     initialize_folder(INPUT_FOLDER)
     delete_folder(OUTPUT_FOLDER)
-    generate_file_copies(1000)
+    generate_file_copies(DATA_FOLDER, INPUT_FOLDER, n)
 
     hadoop(
         input_folder=INPUT_FOLDER,
@@ -19,13 +20,22 @@ def test_01():
         mapper_fn=mapper,
         reducer_fn=reducer,
     )
-
+    
     with open(f"{OUTPUT_FOLDER}/part-00000", "r", encoding="utf-8") as f:
         lines = f.readlines()
-        result = {}
-        for line in lines:
-            key, value = line.strip().split("\t")
-            result[key] = int(value)
+
+    print("\n===== CONTENIDO DE part-00000 =====")
+    print("".join(lines))
+    print("===================================")
+
+    result = {}
+
+    for line in lines:
+        key, value = line.strip().split("\t")
+        result[key] = int(value)
+
+    print("RESULT:", result)
+    print("KEYS:", result.keys())
 
     assert result["analytics"] == 5000
     assert result["business"] == 7000
--- PRE_04_limpieza/src/clean.py
+++ PRE_04_limpieza/src/clean.py
@@ -1,2 +1,79 @@
+import os
+
+import pandas as pd
+
+INPUT_FILE = "PRE_04_limpieza/data/ventas.csv"
+OUTPUT_FILE = "PRE_04_limpieza/submission/ventas.csv"
+
+SUPPLIER_REPLACEMENTS = {
+    "Bancolombia S.A.": [
+        "BANCOLOMBIA S.A.",
+    ],
+    "Corona S.A.S.": [
+        "Corona SAS",
+    ],
+    "Ecopetrol S.A.": [
+        "Ecopetrol  S.A.",
+    ],
+    "Google Colombia Ltda.": [
+        "GOOGLE COLOMBIA LTDA.",
+    ],
+    
+}
+
+
+def make_replacements(series, replacements):
+    for replacement, values in replacements.items():
+        for value in values:
+            series = series.replace(value, replacement)
+    return series
+
+def strip_whitespace(series):
+    return series.str.strip()
+
+def to_lowercase(series):
+    return series.str.lower()
+
+def replace_space_with_underscore(series):
+    return series.str.replace(" ", "_")
+
+def clean_column_names(df):
+    df.columns = strip_whitespace(df.columns)
+    df.columns = to_lowercase(df.columns)
+    df.columns = replace_space_with_underscore(df.columns)
+    return df
+
+
+def clean_supplier_column(series):
+    series = strip_whitespace(series)
+    series = make_replacements(series, SUPPLIER_REPLACEMENTS)
+    return series
+
 def main():
-    pass
+
+    df = pd.read_csv(INPUT_FILE)
+
+    df = clean_column_names(df)
+    
+    df["supplier"] = clean_supplier_column(df["supplier"])
+    
+    df.to_csv(OUTPUT_FILE, index=False)
+
+    
+
+if __name__ == "__main__":
+    main()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
